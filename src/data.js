58dce08fb3562cbbe33f038d83ecf4c8746f1e52
Roman Putilov <Putilov.Roman@gmail.com>
2022-05-09 11:14:39 +0500

    feat: create keyboard data and parse it

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const data = {
+  keys: [
+    // first row
+    [
+      {
+        code: 'tab',
+        ru: 'Tab',
+        en: 'Tab',
+        class: 'tab',
+      },
+      {
+        code: 'q',
+        ru: 'й',
+        en: 'q',
+        class: '',
+      },
+      {
+        code: 'w',
+        ru: 'ц',
+        en: 'w',
+        class: '',
+      },
+    ],
+
+    // second row
+    [
+      {
+        code: 'tab',
+        ru: 'Tab',
+        en: 'Tab',
+        class: 'tab',
+      },
+      {
+        code: 'q',
+        ru: 'й',
+        en: 'q',
+        class: '',
+      },
+      {
+        code: 'w',
+        ru: 'ц',
+        en: 'w',
+        class: '',
+      },
+    ],
+  ],
+};
+
+export default data;
