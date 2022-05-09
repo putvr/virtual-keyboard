7ff6f404392f336445d00c6a076fea897c996d9f
Roman Putilov <Putilov.Roman@gmail.com>
2022-05-09 11:12:40 +0500

    feat: enable eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
+      template: 'src/index.html',
+      filename: 'index.html',
     }),
     new ESLintPlugin({
       extensions: ['js'],
@@ -23,7 +23,7 @@ module.exports = {
     rules: [
       {
         test: /\.(scss|css)$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
